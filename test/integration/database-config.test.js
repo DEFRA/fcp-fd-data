6324d23903e8cc3e63c6e6a0c708d776557d86c7
Simon Dunn <simondunn90@gmail.com>
2024-11-04 08:59:31 +0000

    wip

--- test/integration/database-config.test.js
+++ test/integration/database-config.test.js
@@ -1,15 +1,9 @@
-import { expect, jest } from '@jest/globals'
+import { expect } from '@jest/globals'
 import db from '../../app/data/index.js'
 
-jest.setTimeout(30000)
-
 beforeEach(async () => {
-  try {
-    await db.sequelize.truncate({ cascade: true })
-    await db.initial.create({ message: 'Hello, World!' })
-  } catch (error) {
-    console.log(error)
-  }
+  await db.sequelize.truncate({ cascade: true })
+  await db.initial.create({ message: 'Hello, World!' })
 })
 
 afterEach(async () => {
@@ -19,7 +13,6 @@ afterEach(async () => {
 describe('Database Config Tests', () => {
   test('should return data from the database', async () => {
     const result = await db.initial.findAll()
-    const plainResult = result.map(record => record.get({ plain: true }))
-    expect(plainResult).toEqual([{ id: 1, message: 'Hello, World!' }])
+    expect(result).toHaveLength(1)
   })
 })
