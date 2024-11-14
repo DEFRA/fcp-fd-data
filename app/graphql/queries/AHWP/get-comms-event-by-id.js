0cb6cc8db029c0b37c93130d1594415d475c8d23
JungaBunga <amir.ishmuhametov@gmail.com>
2024-11-14 13:47:12 +0000

    added return statement to commsMessagebyID

--- app/graphql/queries/AHWP/get-comms-event-by-id.js
+++ app/graphql/queries/AHWP/get-comms-event-by-id.js
@@ -1,7 +1,34 @@
 import db from '../../../data/index.js'
 
 const commsEvent = async (_, { id }) => {
-  return await db.commsEvent.findByPk(id)
+  const event = await db.commsEvent.findByPk(id)
+  if (!event) {
+    return null
+  }
+  return {
+    id: event.id,
+    dateCreated: event.dateCreated,
+    commsMessage: {
+      id: event.commsMessage.id,
+      data: {
+        crn: event.commsMessage.data.crn,
+        sbi: event.commsMessage.data.sbi,
+        commsType: event.commsMessage.data.commsType,
+        reference: event.commsMessage.data.reference,
+        commsAddress: event.commsMessage.data.commsAddress,
+        sourceSystem: event.commsMessage.data.sourceSystem,
+        emailReplyToId: event.commsMessage.data.emailReplyToId,
+        statusDetails: event.commsMessage.data.statusDetails,
+        coorelationId: event.commsMessage.data.coorelationId,
+        personalisation: event.commsMessage.data.personalisation
+      },
+      time: event.commsMessage.time,
+      type: event.commsMessage.type,
+      source: event.commsMessage.source,
+      specversion: event.commsMessage.specversion,
+      datacontenttype: event.commsMessage.datacontenttype
+    }
+  }
 }
 
 export default commsEvent
