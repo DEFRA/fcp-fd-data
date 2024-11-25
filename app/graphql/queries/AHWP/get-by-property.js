3f20ed7b966ac10bf7cbf8cc937385a71b916c8a
JungaBunga <amir.ishmuhametov@gmail.com>
2024-11-25 14:53:10 +0000

    working on unit tests

--- app/graphql/queries/AHWP/get-by-property.js
+++ app/graphql/queries/AHWP/get-by-property.js
@@ -1,49 +1,29 @@
 import { Op } from 'sequelize'
 import db from '../../../data/index.js'
 import commsEnumMap from '../../schema/comms-message/comms-enum-map.js'
+import queryCommsEvent from './get-by-property-commsaddress.js'
 
 const commsByProperty = async (_, { key, value }) => {
   const mappedKey = commsEnumMap[key]
   value = value.toString()
 
-  if (mappedKey === 'data,commsAddresses') {
-    try {
-      const result = await db.commsEvent.findAll({
-        where: {
-          [Op.or]: [
-            // Case 1: commsAddresses is a string
-            db.sequelize.where(
-              db.sequelize.json(`commsMessage.${mappedKey}`),
-              { [Op.eq]: value }
-            ),
-            // Case 2: commsAddresses is an array
-            db.sequelize.where(
-              db.sequelize.cast(db.sequelize.json(`commsMessage.${mappedKey}`), 'jsonb'),
-              { [Op.contains]: db.sequelize.literal(`'["${value}"]'::jsonb`) }
-            )
-          ]
-        }
-      })
-
-      console.log('*****', result)
-      return result
-    } catch (error) {
-      throw new Error(`Error executing query: ${error.message}`)
-    }
-  }
-
   try {
-    const result = await db.commsEvent.findAll({
-      where: db.sequelize.where(
-        db.sequelize.cast(db.sequelize.json(`commsMessage.${mappedKey}`), 'TEXT'),
-        {
-          [Op.eq]: value
-        }
-      )
-    })
-
-    return result
+    if (mappedKey === commsEnumMap.COMMS_ADDRESSES) {
+      const result = await queryCommsEvent(mappedKey, value)
+      return result
+    } else {
+      const result = await db.commsEvent.findAll({
+        where: db.sequelize.where(
+          db.sequelize.cast(db.sequelize.json(`commsMessage.${mappedKey}`), 'TEXT'),
+          {
+            [Op.eq]: value
+          }
+        )
+      })
+      return result
+    }
   } catch (error) {
+    console.error(`Error executing query for key: ${key}, mappedKey: ${mappedKey}, value: ${value} - ${error.message}`)
     throw new Error(`Error executing query: ${error.message}`)
   }
 }
