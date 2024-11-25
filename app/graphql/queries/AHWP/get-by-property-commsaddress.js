92acab09697a33e4ebcf6917ca05dd5ed131c010
JungaBunga <amir.ishmuhametov@gmail.com>
2024-11-25 15:44:00 +0000

    added escape character function

--- app/graphql/queries/AHWP/get-by-property-commsaddress.js
+++ app/graphql/queries/AHWP/get-by-property-commsaddress.js
@@ -1,7 +1,12 @@
 import { Op } from 'sequelize'
 import db from '../../../data/index.js'
 
+const escapeLikePattern = (str) => {
+  return str.replace(/[%_\\]/g, '\\$&')
+}
+
 const queryCommsEvent = async (mappedKey, value) => {
+  const escapedValue = escapeLikePattern(value)
   return await db.commsEvent.findAll({
     where: {
       [Op.or]: [
@@ -14,11 +19,12 @@ const queryCommsEvent = async (mappedKey, value) => {
         db.sequelize.where(
           db.sequelize.json(`commsMessage.${mappedKey}`),
           {
-            [Op.iLike]: `%${value}%`
+            [Op.iLike]: `%${escapedValue}%`
           }
         )
       ]
     }
   })
 }
+
 export default queryCommsEvent
