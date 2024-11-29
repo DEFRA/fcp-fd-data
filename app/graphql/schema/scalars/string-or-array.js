9435a918adeddbda7f82c7fbed761232d1eadb4f
Simon Dunn <simondunn90@gmail.com>
2024-11-29 16:22:46 +0000

    refactor AGAIN

--- app/graphql/schema/scalars/string-or-array.js
+++ app/graphql/schema/scalars/string-or-array.js
@@ -4,19 +4,24 @@ const validate = (value) => {
   if (typeof value === 'string' || (Array.isArray(value) && value.every(item => typeof item === 'string'))) {
     return { value }
   }
-  throw new Error('Value must be either a string or an array of strings')
 }
 
 const StringOrArray = new GraphQLScalarType({
   name: 'StringOrArray',
   description: 'Custom scalar that can be either a string or an array of strings',
   parseValue (value) {
-    const { value: validatedValue } = validate(value)
-    return validatedValue
+    const validatedValue = validate(value)
+    if (validatedValue === undefined) {
+      throw new Error('Value must be either a string or an array of strings')
+    }
+    return validatedValue.value
   },
   serialize (value) {
-    const { value: validatedValue } = validate(value)
-    return validatedValue
+    const validatedValue = validate(value)
+    if (validatedValue === undefined) {
+      throw new Error('Value must be either a string or an array of strings')
+    }
+    return validatedValue.value
   },
   parseLiteral (ast) {
     if (ast.kind === Kind.STRING) {
