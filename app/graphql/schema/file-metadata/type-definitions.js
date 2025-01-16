0e1950fa4337eedfe0b05f63b2917472d98cee76
Adam Kay <thousand_leaves@hotmail.com>
2025-01-16 10:42:48 +0000

    adds annotation comments to type-definitions

--- app/graphql/schema/file-metadata/type-definitions.js
+++ app/graphql/schema/file-metadata/type-definitions.js
@@ -12,37 +12,58 @@ type Query {
   """
   getMetadata(key: fileMetadataEnum!, value: StringOrArray!): [FileMetadata]
 }
+
 """ Metadata received """
 type FileMetadata {
+  """ Unique identifier for the file metadata """
   id: String
+
+  """ Date and time when metadata has been saved in fd-data database """
   dateCreated: Timestamp
+
+  """ Details of the metadata """
   metadata: metadataDetails
 }
 
+""" Details of the metadata """
 type metadataDetails {
+  """ Unique identifier for the metadata """
   id: String
+
+  """ Structured business data related to the metadata """
   data: metadata
+
+  """ Time of the metadata """
   time: Timestamp
+
+  """ Type of the metadata """
   type: String
+
+  """ Source system of the metadata """
   source: String
+
+  """ Specification version of the metadata (Cloud Events format) """
   specversion: String
+
+  """ Content type of the metadata data """
   datacontenttype: String
 }
 
+""" Data structure for metadata content """
 type metadata {
   """ Single Business Identifier """
   sbi: Int
+
   """ Blob Reference """
   blobReference: String
 }
 
 """ Enumeration for file metadata """
 enum fileMetadataEnum {
-
   """ Single Business Identifier """
   SBI
 
-  """ Blob reference """
+  """ Blob Reference """
   BLOB_REFERENCE
 }
 `
