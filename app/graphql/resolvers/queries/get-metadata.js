34581b71335c3274e4fae84246a4d662c618e2de
Adam Kay <thousand_leaves@hotmail.com>
2025-01-17 15:42:52 +0000

    refactors resolver

--- app/graphql/resolvers/queries/get-metadata.js
+++ app/graphql/resolvers/queries/get-metadata.js
@@ -5,17 +5,30 @@ const getMetadata = async (_, { key, value }) => {
   const mappedKey = enumMap[key]
   value = value.toString()
 
-  console.log('Key:', key)
-  console.log('Mapped Key:', mappedKey)
-  console.log('Value:', value)
-
-  const result = await db.fileMetadata.findAll({
+  const response = await db.fileMetadata.findAll({
     where: { [`metadata.${mappedKey}`]: value }
   })
 
-  console.log('Query Result:', result)
+  const mappedResponse = response.map((element) => {
+    return {
+      id: element.dataValues.id,
+      dateCreated: element.dataValues.dataCreated,
+      metadata: {
+        id: element.dataValues.metadata.id,
+        data: {
+          sbi: element.dataValues.metadata.data.sbi,
+          blobReference: element.dataValues.metadata.data.blobReference
+        },
+        time: element.dataValues.metadata.time,
+        type: element.dataValues.metadata.type,
+        source: element.dataValues.metadata.source,
+        specversion: element.dataValues.metadata.specversion,
+        datacontenttype: element.dataValues.metadata.datacontenttype
+      }
+    }
+  })
 
-  return result
+  return mappedResponse
 }
 
 export default getMetadata
