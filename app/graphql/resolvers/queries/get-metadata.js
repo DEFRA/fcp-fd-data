8acf7608cc7ae55ead1d97ddc94accd0a5600d6d
Adam Kay <thousand_leaves@hotmail.com>
2025-01-21 15:11:36 +0000

    refactor getMetadata query

--- app/graphql/resolvers/queries/get-metadata.js
+++ app/graphql/resolvers/queries/get-metadata.js
@@ -15,26 +15,22 @@ const getMetadata = async (_, { key, value }) => {
     }
   })
 
-  const mappedResponse = response.map((element) => {
-    return {
-      id: element.dataValues.id,
-      dateCreated: element.dataValues.dataCreated,
-      metadata: {
-        id: element.dataValues.metadata.id,
-        data: {
-          sbi: element.dataValues.metadata.data.sbi,
-          blobReference: element.dataValues.metadata.data.blobReference
-        },
-        time: element.dataValues.metadata.time,
-        type: element.dataValues.metadata.type,
-        source: element.dataValues.metadata.source,
-        specversion: element.dataValues.metadata.specversion,
-        datacontenttype: element.dataValues.metadata.datacontenttype
-      }
+  return response.map(element => ({
+    id: element.dataValues.id,
+    dateCreated: element.dataValues.dataCreated,
+    metadata: {
+      id: element.dataValues.metadata.id,
+      data: {
+        sbi: element.dataValues.metadata.data.sbi,
+        blobReference: element.dataValues.metadata.data.blobReference
+      },
+      time: element.dataValues.metadata.time,
+      type: element.dataValues.metadata.type,
+      source: element.dataValues.metadata.source,
+      specversion: element.dataValues.metadata.specversion,
+      datacontenttype: element.dataValues.metadata.datacontenttype
     }
-  })
-
-  return mappedResponse
+  }))
 }
 
 export default getMetadata
