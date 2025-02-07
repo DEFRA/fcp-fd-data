3e070cdb1af23cbb455e2f2709c876fcfd86ddbd
Simon Dunn <simondunn90@gmail.com>
2025-02-07 12:07:05 +0000

    WIP use raw query

--- app/graphql/resolvers/comms-message/get-comms-event-by-property.js
+++ app/graphql/resolvers/comms-message/get-comms-event-by-property.js
@@ -1,46 +1,72 @@
 import db from '../../../data/index.js'
 import enumMap from '../../schema/comms-message/enum-map.js'
-import { Op } from 'sequelize'
+
+const valuesMap = values => `ARRAY[${values.map(val => `'${val}'`).join(', ')}]`
+
+const whereClause = (mappedKey, values) => {
+  if (mappedKey.split('.').length === 1) {
+    return `"commsMessage"->>'${mappedKey}' = '${values[0]}'`
+  }
+  return `"commsMessage"->'${mappedKey.split('.')[0]}'->'${mappedKey.split('.')[1]}' ?| ${valuesMap(values)} `
+}
 
 const getCommsEventByProperty = async (_, { key, value }) => {
   const mappedKey = enumMap[key]
   const values = Array.isArray(value) ? value.map(String) : [String(value)]
 
-  const commsEventRecords = await db.commsEvent.findAll({
-    where: {
-      [Op.or]: [
-        { [`commsMessage.${mappedKey}`]: { [Op.in]: values } },
-        { [`commsMessage.${mappedKey}`]: { [Op.contains]: JSON.stringify(values) } }
-      ]
-    }
-  })
+  try {
+    const query =
+     `SELECT *
+      FROM "public"."commsEvent" AS "commsEvent"
+      WHERE ${whereClause(mappedKey, values)}`
 
-  return commsEventRecords.map(record => ({
-    id: record.dataValues.id,
-    dateCreated: record.dataValues.dateCreated,
-    commsMessage: {
-      id: record.dataValues.commsMessage.id,
-      time: record.dataValues.commsMessage.time,
-      type: record.dataValues.commsMessage.type,
-      source: record.dataValues.commsMessage.source,
-      specversion: record.dataValues.commsMessage.specversion,
-      datacontenttype: record.dataValues.commsMessage.datacontenttype,
-      data: {
-        commsType: record.dataValues.commsMessage.data.commsType,
-        commsAddresses: record.dataValues.commsMessage.data.commsAddresses,
-        correlationId: record.dataValues.commsMessage.data.correlationId,
-        crn: record.dataValues.commsMessage.data.crn,
-        emailReplyToId: record.dataValues.commsMessage.data.emailReplyToId,
-        notifyTemplateId: record.dataValues.commsMessage.data.notifyTemplateId,
-        oneClickUnsubscribeUrl: record.dataValues.commsMessage.data.oneClickUnsubscribeUrl,
-        personalisation: record.dataValues.commsMessage.data.personalisation,
-        reference: record.dataValues.commsMessage.data.reference,
-        sbi: record.dataValues.commsMessage.data.sbi,
-        sourceSystem: record.dataValues.commsMessage.data.sourceSystem,
-        statusDetails: record.dataValues.commsMessage.data.statusDetails
+    const commsEventRecords = await db.sequelize.query(query, {
+      type: db.sequelize.QueryTypes.SELECT,
+      model: db.commsEvent
+    })
+    // const commsEventRecords = await db.commsEvent.findAll({
+    //   where: {
+    //     [`commsMessage.${mappedKey}`]: {
+    //       [Op.anyKeyExists]: values::jsonb
+
+    //     }
+    //     // [Op.or]: [
+    //     //   // { [`commsMessage.${mappedKey}`]: { [Op.in]: values } },
+    //     //   // { [`commsMessage.${mappedKey}`]: { [Op.contains]: JSON.stringify(§) } },
+    //     // ]
+    //   }
+    // })
+
+    return commsEventRecords.map(record => ({
+      id: record.dataValues.id,
+      dateCreated: record.dataValues.dateCreated,
+      commsMessage: {
+        id: record.dataValues.commsMessage.id,
+        time: record.dataValues.commsMessage.time,
+        type: record.dataValues.commsMessage.type,
+        source: record.dataValues.commsMessage.source,
+        specversion: record.dataValues.commsMessage.specversion,
+        datacontenttype: record.dataValues.commsMessage.datacontenttype,
+        data: {
+          commsType: record.dataValues.commsMessage.data.commsType,
+          commsAddresses: record.dataValues.commsMessage.data.commsAddresses,
+          correlationId: record.dataValues.commsMessage.data.correlationId,
+          crn: record.dataValues.commsMessage.data.crn,
+          emailReplyToId: record.dataValues.commsMessage.data.emailReplyToId,
+          notifyTemplateId: record.dataValues.commsMessage.data.notifyTemplateId,
+          oneClickUnsubscribeUrl: record.dataValues.commsMessage.data.oneClickUnsubscribeUrl,
+          personalisation: record.dataValues.commsMessage.data.personalisation,
+          reference: record.dataValues.commsMessage.data.reference,
+          sbi: record.dataValues.commsMessage.data.sbi,
+          sourceSystem: record.dataValues.commsMessage.data.sourceSystem,
+          statusDetails: record.dataValues.commsMessage.data.statusDetails
+        }
       }
-    }
-  }))
+    }))
+  } catch (error) {
+    console.error('Error fetching comms event by property', error)
+    throw new Error(error.message)
+  }
 }
 
 export default getCommsEventByProperty
