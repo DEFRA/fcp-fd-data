8beaf655de97f9bc4a311abd081a04b1025b0657
Simon Dunn <simondunn90@gmail.com>
2025-02-07 12:07:35 +0000

    update test to use db.sequelize to account for raw query

--- test/unit/graphql/queries/query-event-by-property-throw-error.test.js
+++ test/unit/graphql/queries/query-event-by-property-throw-error.test.js
@@ -5,11 +5,11 @@ import { jest } from '@jest/globals'
 describe('commsByProperty query unit test', () => {
   test('should throw an error when sequelize execution fails', async () => {
     const mockError = new Error('Query execution failed')
-    jest.spyOn(db.commsEvent, 'findAll').mockImplementation(() => {
+    jest.spyOn(db.sequelize, 'query').mockImplementation(() => {
       throw mockError
     })
 
-    const result = async () => { await getCommsEventByProperty(null, { key: 'someKey', value: 'someValue' }) }
+    const result = async () => { await getCommsEventByProperty(null, { key: 'SOURCE', value: 'someValue' }) }
     expect(result).rejects.toThrowError('Query execution failed')
   })
 })
